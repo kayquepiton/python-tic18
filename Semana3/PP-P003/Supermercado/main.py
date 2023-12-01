b50bb0b17e7e63570570e94ee679eb10b88f1f4f
kayquepiton <kayquepiton@gmail.com>
2023-11-30 23:32:25 -0300

    PP-P003

--- Semana3/PP-P003/Supermercado/main.py
+++ Semana3/PP-P003/Supermercado/main.py
@@ -1,13 +1,19 @@
 import os
 
+CARACTERES_CODIGO = 13
+PRODUTOS_POR_VEZ = 10
+
+def verificar_nome(nome):
+    return bool(nome and nome[0].isupper())
+
 def inserir_produto(produtos):
     codigo = input("\nDigite o código do produto (13 caracteres): ")
-    while len(codigo) != 13:
-        print("Código inválido. Deve conter exatamente 13 caracteres.")
+    while len(codigo) != CARACTERES_CODIGO:
+        print(f"Código inválido. Deve conter exatamente {CARACTERES_CODIGO} caracteres.")
         codigo = input("Digite o código do produto (13 caracteres): ")
 
     nome = input("Digite o nome do produto: ")
-    while not nome or not nome[0].isupper():
+    while not verificar_nome(nome):
         print("O nome do produto deve começar com uma letra maiúscula.")
         nome = input("Digite o nome do produto: ")
 
@@ -29,10 +35,15 @@ def excluir_produto(produtos):
             return
     print("Produto não encontrado.")
 
+def ordenar_por_preco(produtos):
+    return sorted(produtos, key=lambda x: x['preco'])
+
 def listar_produtos(produtos):
-    for i, produto in enumerate(produtos):
+    produtos_ordenados = ordenar_por_preco(produtos)
+    
+    for i, produto in enumerate(produtos_ordenados):
         print(f"{i + 1}. Código: {produto['codigo']}, Nome: {produto['nome']}, Preço: R${produto['preco']:.2f}")
-        if (i + 1) % 10 == 0 and (i + 1) != len(produtos):
+        if (i + 1) % PRODUTOS_POR_VEZ == 0 and (i + 1) != len(produtos_ordenados):
             input("Pressione Enter para ver mais produtos...")
 
 def consultar_preco(produtos):
@@ -43,17 +54,19 @@ def consultar_preco(produtos):
             return
     print("Produto não encontrado.")
 
+def imprimir_menu():
+    print("\n===== Menu de Opções =====")
+    print("1. Inserir novo produto")
+    print("2. Excluir produto cadastrado")
+    print("3. Listar todos os produtos")
+    print("4. Consultar preço de um produto")
+    print("0. Sair do programa")
+
 def principal():
     produtos = []
 
     while True:
-        print("\n===== Menu de Opções =====")
-        print("1. Inserir novo produto")
-        print("2. Excluir produto cadastrado")
-        print("3. Listar todos os produtos")
-        print("4. Consultar preço de um produto")
-        print("0. Sair do programa")
-
+        imprimir_menu()
         opcao = input("Escolha uma opção: ")
 
         if opcao == '1':
